4398517db7d45c0c08f63f2249c854a615199387
Orlin Alvarado <oralvarado1@gmail.com>
2021-10-30 22:02:07 -0600

    Preparando para publicar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:peliculas/src/pages/home_page.dart';
 import 'package:peliculas/src/pages/pelicula_detalle.dart';
  
-void main() => runApp(MyApp());
+void main() {
+  
+  return runApp(MyApp());
+} 
  
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    
+SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Películas',
